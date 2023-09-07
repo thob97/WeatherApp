a6d2cb46edce711311d8994f74b9fa01925c85b3
Thore Brehmer <thore.brehmer@fu-berlin.de>
2023-09-07 16:15:00 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Thore Brehmer on 07.09.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
